e8a686292271eebb65b48397445d110912abb72b
MagicPika <154286904+MagicPika@users.noreply.github.com>
2026-03-01 08:31:42 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from modules import applications
+import config
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Подключаем обработчик заявок
+bot.add_cog(applications.Applications(bot))
+
+@bot.event
+async def on_ready():
+    print(f"Бот готов → {bot.user}")
+    await bot.tree.sync()
+    print("Слэш-команды синхронизированы")
+
+bot.run(config.TOKEN)
