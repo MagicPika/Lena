44c5316ff27a5fcf5858c68457c2ee14aba558af
MagicPika <154286904+MagicPika@users.noreply.github.com>
2026-03-01 16:05:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
+import os
 import discord
 from discord.ext import commands
-from modules.applications import Applications
-import config
+
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
-intents.members = True
 intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Подключаем обработчик заявок
-bot.add_cog(applications.Applications(bot))
-
 @bot.event
 async def on_ready():
-    print(f"Бот готов → {bot.user}")
-    await bot.tree.sync()
-    print("Слэш-команды синхронизированы")
+    print(f"Бот запущен: {bot.user}")
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
 
-bot.run(config.TOKEN)
-
+bot.run(TOKEN)
